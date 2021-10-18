14536b73af2d0d2c517d024d949dfb0b6cf8b230
M. Khorram <m.khorram5@gmail.com>
2021-10-18 18:57:56 +0330

    some works about ringbuffer

--- src/ringbuffer.hpp
+++ src/ringbuffer.hpp
@@ -15,26 +15,35 @@ private:
 public:
     RingBuffer(const RingBuffer&) = delete;
     RingBuffer& operator=(const RingBuffer&) = delete;
-    RingBuffer(unsigned long bufSize = 100000) : m_bufSize(bufSize) { m_buffer.reserve(m_bufSize+1); }
+    RingBuffer(unsigned long bufSize = 100000) : m_bufSize(bufSize) { m_buffer.reserve(m_bufSize); }
 
     inline unsigned long lenghtToRead() { return (m_bufSize+m_writePoint-m_readPoint)%m_bufSize; }
+
     void haveRead(unsigned long length)
     {
         if (lenghtToRead() < length)
-            throw ;
+            throw "The length is beyond the available data";
         m_readPoint = (m_readPoint+length)%m_bufSize;
+    }
+
+    void insert(T& copyableVar)
+    {
+        if (m_bufSize - lenghtToRead() == 1)
+            ++m_readPoint;
+        m_buffer[m_writePoint] = copyableVar;
+        ++m_writePoint;
     }
 
     T& operator[](unsigned long index)
     {
         if ( lenghtToRead() < index ) // how much data is available to read
-            throw;
+            throw "The index is beyond the available data";
 
         unsigned long idx = (index + m_readPoint) % m_bufSize;
         return m_buffer[idx];
     }
 
-    int getLength() const { return m_bufSize; }
+    unsigned long getLength() const { return m_bufSize; }
 };
 
 #endif // RINGBUFFER_H
