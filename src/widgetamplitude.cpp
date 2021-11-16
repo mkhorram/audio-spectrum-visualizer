9375bf70e787ff67354bc3d01f91f24fe8419bd9
M. Khorram <m.khorram5@gmail.com>
2021-11-16 10:23:33 +0330

    initial works on widgetamplitude

--- src/widgetamplitude.cpp
+++ src/widgetamplitude.cpp
@@ -1,18 +1,31 @@
 #include "widgetamplitude.h"
 
-WidgetAmplitude::WidgetAmplitude(QWidget *parent) : QWidget(parent), m_amplitudes(5000)
+WidgetAmplitude::WidgetAmplitude(QWidget *parent, unsigned long bufSize) : QWidget(parent), m_amplitudes(bufSize)
 {
 
 }
 
-void WidgetAmplitude::setLevelRagne(float minValue, float maxValue)
+void WidgetAmplitude::setLevelRagne(double rangeMinValue, double rangeMaxValue)
 {
-    m_minValue = minValue;
-    m_maxValue = maxValue;
+    m_rangeMinValue = rangeMinValue;
+    m_rangeMaxValue = rangeMaxValue;
 }
 
-void WidgetAmplitude::insertLevelBar(float lowInput, float highInput)
+void WidgetAmplitude::insertLevelBar(double lowInput, double highInput)
 {
     BarAmplitudeValues bav(lowInput, highInput);
     m_amplitudes.insert(bav);
+}
+
+void WidgetAmplitude::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+
+    int drawLeftEdge = painter.viewport().left() + m_leftMargin;
+    int drawTopEdge = painter.viewport().top() + m_topMargin;
+    int drawWidth = painter.viewport().right() - (m_rightMargin+m_leftMargin);
+    int drawHeight = painter.viewport().bottom() - (m_bottomMargin+m_topMargin);
+
+    painter.setPen(Qt::black);
+    painter.drawRect(drawLeftEdge, drawTopEdge, drawWidth, drawHeight);
 }
--- src/widgetamplitude.h
+++ src/widgetamplitude.h
@@ -2,6 +2,7 @@
 #define WIDGETAMPLITUDE_H
 
 #include <QWidget>
+#include <QPainter>
 
 #include "ringbuffer.hpp"
 
@@ -10,29 +11,41 @@ class WidgetAmplitude : public QWidget
     Q_OBJECT
     struct BarAmplitudeValues
     {
-        BarAmplitudeValues(float low_Input, float high_Input)
+        BarAmplitudeValues(double low_Input = 0, double high_Input = 0)
         {
             lowInput = low_Input;
             highInput = high_Input;
         }
-        float lowInput;
-        float highInput;
+        double lowInput;
+        double highInput;
     };
 
-public:
-    explicit WidgetAmplitude(QWidget *parent = nullptr);
+private:
+    double m_rangeMinValue;
+    double m_rangeMaxValue;
+    RingBuffer<BarAmplitudeValues> m_amplitudes;
 
-    void setLevelRagne(float minValue, float maxValue);
-    void insertLevelBar(float lowInput, float highInput);
+    int m_leftMargin = 10;
+    int m_rightMargin = 10;
+    int m_topMargin = 10;
+    int m_bottomMargin = 10;
+
+    int rowHight = 2;
+    int newRowHight = 5;
+
+public:
+    explicit WidgetAmplitude(QWidget *parent = nullptr, unsigned long bufSize = 5000);
+
+    void setLevelRagne(double rangeMinValue, double rangeMaxValue);
+    void insertLevelBar(double lowInput, double highInput);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
 
 signals:
 
 public slots:
 
-private:
-    float m_minValue;
-    float m_maxValue;
-    RingBuffer<BarAmplitudeValues> m_amplitudes;
 };
 
 #endif // WIDGETAMPLITUDE_H
