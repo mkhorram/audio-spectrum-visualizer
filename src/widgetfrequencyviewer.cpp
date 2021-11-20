5f515e559c06ed38abed0a638ca673a12042f028
M. Khorram <m.khorram5@gmail.com>
2021-11-20 22:33:31 +0330

    begin to work on widgetfrequencyviewer

--- src/widgetfrequencyviewer.cpp
+++ src/widgetfrequencyviewer.cpp
@@ -2,5 +2,23 @@
 
 WidgetFrequencyViewer::WidgetFrequencyViewer(QWidget *parent) : QWidget(parent)
 {
+    m_imageBuffer = QImage(this->width(), this->height(), QImage::Format_RGB32);
+}
 
+void WidgetFrequencyViewer::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    painter.drawImage(QPoint(0,0), m_imageBuffer);
+}
+
+void WidgetFrequencyViewer::resizeEvent(QResizeEvent *event)
+{
+    m_imageBuffer = QImage(event->size().width(), event->size().height(), QImage::Format_RGB32);
+    QColor color((event->size().width()*5)%256, event->size().height()%256, (event->size().width()+event->size().height())%255);
+    m_imageBuffer.fill(color);
+}
+
+void WidgetFrequencyViewer::insertNewSpectrumRow(std::shared_ptr<std::vector<std::complex<double> > > FFTOutput, double ratio)
+{
+    //
 }
--- src/widgetfrequencyviewer.h
+++ src/widgetfrequencyviewer.h
@@ -1,17 +1,32 @@
 #ifndef WIDGETFREQUENCYVIEWER_H
 #define WIDGETFREQUENCYVIEWER_H
 
+#include <QImage>
+#include <QPainter>
+#include <QResizeEvent>
 #include <QWidget>
+
+#include <complex>
+#include <memory>
+#include <vector>
 
 class WidgetFrequencyViewer : public QWidget
 {
     Q_OBJECT
+private:
+    QImage m_imageBuffer;
+
 public:
     explicit WidgetFrequencyViewer(QWidget *parent = nullptr);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
 
 signals:
 
 public slots:
+    void insertNewSpectrumRow(std::shared_ptr<std::vector<std::complex<double>>> FFTOutput, double ratio);
 };
 
 #endif // WIDGETFREQUENCYVIEWER_H
